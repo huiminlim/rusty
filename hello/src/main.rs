84916b22812f15e6aaab9ced0b89149419a78d22
huiminlim <e0036379@u.nus.edu>
2024-03-02 16:38:58 +0800

    add number data type

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -35,5 +35,12 @@ fn greet_world() {
         if let Ok(length) = fields[1].parse::<f32>() {
             println!("{}, {}cm", name, length);
         }
+
+        let a: i32 = 10;
+        let b: u16 = 100;
+        let b_ = b.try_into().unwrap();
+        if a < b_ {
+            println!("Ten is less than one hundred.");
+        }
    }
 }
